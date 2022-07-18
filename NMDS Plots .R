8b4a7fe86d39ce0d42406051f228e9493da1a0e4
diportugal <dportugal8@gmail.com>
2022-07-17 22:20:41 -0400

    New file.

--- NMDS Plots .R
+++ NMDS Plots .R
@@ -1,7 +1,36 @@
 # Other plot types 
 
 
+#PHYLOSEQ OBJECTS####
+#phyloPEA_PA
+#phyloRFTMPEA_PA
+#phyloRFTM_PA
+#phyloSITEOYNW_PA
+#phyloSITESWNW_PA
+#phyloRFTMOY_PA
+#phyloRFTMSW_PA
+#phyloPEAOY_PA
+#phyloPEASW_PA
 
+
+##### Plot Richness for RFTM_PA####
+plot_richness(phyloRFTM_PA, x="RFTM_pa", measures = c("Observed", "Chao", "Simpson", "Shannon"))+
+  geom_boxplot()+
+  stat_compare_means(method = "t.test", label.y = 0)+
+  theme(legend.position="right", legend.text=element_text(size=8),
+        axis.ticks.x = element_blank(), 
+        axis.line = element_line(color="black"), text = element_text(size=10), 
+        plot.title = element_text(family = "sans", face = "bold", hjust = 0.5, size = 15, colour = "black", margin=margin(15, 15, 0, 15)), 
+        plot.subtitle = element_text(family = "sans", face = "bold", hjust = 0.5, color = "black", margin=margin(15, 15, 15, 15), ),
+        plot.caption = element_text(color = "black", face = "italic", margin=margin(15, 15, 15, 15)),
+        axis.title.x = element_text(family = "sans", size = 15, margin=margin(15, 15, 15, 15)), 
+        axis.title.y = element_text(family = "sans", size = 15, margin=margin(15, 15, 15, 15)))+
+  labs(title = "Microbial Richness ", 
+       subtitle = "RFTM Prescence Absence",
+       caption = "Data source: Oyster 16s 2017",
+       x = "RFTM Prescence Absence",
+       y = "Richness")
+ggsave(filename = "Rich_RFTMpa.jpeg", plot=last_plot(), path ="PEA_RFTM/G_RFTM_pa/", width = 15, height = 10)
 
 
 
--- New Phyloseq Object + Abundance Plots.R
+++ New Phyloseq Object + Abundance Plots.R
@@ -13,7 +13,6 @@ physeq
 
     
 # CREATING NEW PHYLOSEQ OBJECTS ####
-
 ##peacrabs.f_1_vs_0
 PEApa_comp <- read.csv("Reduced Tax Tables/PresAbs_PEA_COMP.csv") 
 PEApa_comp <- as.matrix(PEApa_comp) 
@@ -330,26 +329,6 @@ plot_bar(phyloPEASW_PA, fill="Class")+
 ggsave(filename = "Abun_PEASWpa_class.jpeg", plot=last_plot(), path ="PEA_RFTM/G_RFTM_pa/", width = 15, height = 10)
 
 
-
-
-##### Plot Richness for RFTM_PA####
-plot_richness(phyloRFTM_PA, x="RFTM_pa", measures = c("Observed", "Chao", "Simpson", "Shannon"))+
-  geom_boxplot()+
-  stat_compare_means(method = "t.test", label.y = 0)+
-  theme(legend.position="right", legend.text=element_text(size=8),
-        axis.ticks.x = element_blank(), 
-        axis.line = element_line(color="black"), text = element_text(size=10), 
-        plot.title = element_text(family = "sans", face = "bold", hjust = 0.5, size = 15, colour = "black", margin=margin(15, 15, 0, 15)), 
-        plot.subtitle = element_text(family = "sans", face = "bold", hjust = 0.5, color = "black", margin=margin(15, 15, 15, 15), ),
-        plot.caption = element_text(color = "black", face = "italic", margin=margin(15, 15, 15, 15)),
-        axis.title.x = element_text(family = "sans", size = 15, margin=margin(15, 15, 15, 15)), 
-        axis.title.y = element_text(family = "sans", size = 15, margin=margin(15, 15, 15, 15)))+
-  labs(title = "Microbial Richness ", 
-       subtitle = "RFTM Prescence Absence",
-       caption = "Data source: Oyster 16s 2017",
-       x = "RFTM Prescence Absence",
-       y = "Richness")
-ggsave(filename = "Rich_RFTMpa.jpeg", plot=last_plot(), path ="PEA_RFTM/G_RFTM_pa/", width = 15, height = 10)
 
 
 
